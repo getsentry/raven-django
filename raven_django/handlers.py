49cf380d10801ddbcda41f3384caabdee5cbd421
Xavier Ordoquy <xordoquy@linovia.com>
2014-07-26 11:26:44 +0200

    Here's the real thing !

---
+++ raven_django/handlers.py
@@ -0,0 +1,29 @@
+"""
+raven_django.handlers
+~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+import logging
+from raven.handlers.logging import SentryHandler as BaseSentryHandler
+
+
+class SentryHandler(BaseSentryHandler):
+    def __init__(self, level=logging.NOTSET):
+        logging.Handler.__init__(self, level=level)
+
+    def _get_client(self):
+        from raven_django.models import client
+
+        return client
+
+    client = property(_get_client)
+
+    def _emit(self, record):
+        request = getattr(record, 'request', None)
+
+        return super(SentryHandler, self)._emit(record, request=request)
---
+++ raven_django/templatetags/raven.py
@@ -0,0 +1,19 @@
+"""
+raven_django.templatetags.raven
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2013 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def sentry_public_dsn(scheme=None):
+    from raven_django.models import client
+    return client.get_public_dsn(scheme) or ''
